7e30bb87c46464bf39805b66f6248fe4eb4632a6
Rowena <flavinhamantunes@gmail.com>
2017-05-31 05:36:04 -0300

    alterado5

--- SoftwareSRF/src/es/TelaInicio.java
+++ SoftwareSRF/src/es/TelaInicio.java
@@ -22,7 +22,7 @@ public class TelaInicio extends javax.swing.JFrame {
      */
     public TelaInicio() {
         initComponents();
-        setIconImage(new ImageIcon(getClass().getResource("/es/imagens/logomt.png")).getImage());
+        setIconImage(new ImageIcon(getClass().getResource("/es/imagens/logomtbranco.png")).getImage());
         setLocationRelativeTo(null);
         //this.setExtendedState(this.getExtendedState()|JFrame.MAXIMIZED_BOTH);
 
